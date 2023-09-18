b3b3df2895f1a8427fcd82fdf4b18790977396c7
Aleksandr <al.losev.k@gmail.com>
2023-09-18 15:56:53 +0300

    Alpha. Only visual

--- app/js/app.js
+++ app/js/app.js
@@ -13,26 +13,40 @@ $(document).ready(function() {
   }
 
   $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    // centerMode: true,
-    // focusOnSelect: true,
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
-  });
-
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   fade: true,
+   asNavFor: '.slider-nav',
+   dots: true,
+   arrows: true,
+   responsive: [
+     {
+      breakpoint: 600,
+      settings: {
+        arrows: false
+      }
+    }
+   ]
+ });
+ $('.slider-nav').slick({
+   slidesToShow: 7,
+   slidesToScroll: 1,
+   asNavFor: '.slider-for',
+   dots: false,
+   arrows: false,
+   focusOnSelect: true,
+   centerMode: false,
+   responsive: [
+     {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '10px',
+        focusOnSelect: true
+      }
+    }
+   ]
+ });
 })
